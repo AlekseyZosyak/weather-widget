72ad14351d051f6dcfe418dc6dfccec441854c7e
Alexey Zosyak <megazuzka11@gmail.com>
2024-11-07 23:09:02 +0200

    scss

--- dist/bundle.js
+++ dist/bundle.js
@@ -87,7 +87,7 @@ const getRequst = async (url) => {
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
 (() => {
 /*!**************************!*\
   !*** ./src/js/script.js ***!
